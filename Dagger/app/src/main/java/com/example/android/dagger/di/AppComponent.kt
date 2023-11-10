53d9a2ba6fab87e11adf69b72d2891209ae7cff3
Undiy <undiy@yandex.ru>
2023-11-10 16:17:31 +0500

    Dagger: subcomponent scope

--- Dagger/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ Dagger/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,6 +3,7 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
@@ -10,7 +11,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
     // Factory to create instances of the AppComponent
     @Component.Factory
@@ -19,9 +20,9 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
+
     // Classes that can be injected by this Component
-    fun inject(activity: RegistrationActivity)
-    fun inject(fragment: EnterDetailsFragment)
-    fun inject(fragment: TermsAndConditionsFragment)
     fun inject(activity: MainActivity)
 }
